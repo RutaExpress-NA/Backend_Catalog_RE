cbf39ff4025bfc6ff049eb7ff1d5e3e772e5d2c7
NACV <na.cifva7@gmail.comm>
2026-09-19 23:17:49 -0300

    Creacion de modelo Servicio de envio catalogo

---
+++ ms-rutaexpress-catalog/src/main/java/com/na/rutaexpress/catalog/model/ServiceShipping.java
@@ -0,0 +1,49 @@
+package com.na.rutaexpress.catalog.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "service_shipping")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+
+public class ServiceShipping {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false)
+    private String name;
+
+    @Column(nullable=false)
+    private String description;
+    
+    @Column(nullable=false)
+    private Integer price;
+
+    @Column(nullable=false)
+    private Boolean active;
+
+    @Column(nullable=false)
+    private String vehicleTypeRequired;
+
+    public ServiceShipping(String name, String description, Integer price, Boolean active, String vehicleTypeRequired) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.active = active;
+        this.vehicleTypeRequired = vehicleTypeRequired;
+    }
+}
